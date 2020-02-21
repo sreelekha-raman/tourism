f80a99dceecc27c75c918493c75a6d5ee76a7576
sreelekha-raman <60920229+sreelekha-raman@users.noreply.github.com>
2020-02-21 16:26:36 +0530

    Add files via upload

--- validation.js
+++ validation.js
@@ -1,49 +1,49 @@
 
 <script>
-function validate()								 
-{ 
+function validate()
+{
 	var fname = document.forms["supform"]["fname"];
-	var lname = document.forms["supform"]["lname"];			 
-	var email = document.forms["supform"]["email"]; 
-	var number = document.forms["supform"]["number"]; 
-	
-	var password = document.forms["supform"]["password"]; 
-	
+	var lname = document.forms["supform"]["lname"];
+	var email = document.forms["supform"]["email"];
+	var number = document.forms["supform"]["number"];
 
-	if (fname.value == "")								 
-	{ 
-		window.alert("Please enter your name."); 
-		name.focus(); 
-		return false; 
-	} 
+	var password = document.forms["supform"]["password"];
 
-	if (lname.value == "")								 
-	{ 
-		window.alert("Please enter your name."); 
-		name.focus(); 
-		return false; 
-	} 
-	
-	if (email.value == "")								 
-	{ 
-		window.alert("Please enter a valid e-mail address."); 
-		email.focus(); 
-		return false; 
-	} 
 
-	if (number.value == "")						 
-	{ 
-		window.alert("Please enter your telephone number."); 
-		phone.focus(); 
-		return false; 
-	} 
+	if (fname.value == "")
+	{
+		window.alert("Please enter your name.");
+		name.focus();
+		return false;
+	}
 
-	if (password.value == "")					 
-	{ 
-		window.alert("Please enter your password"); 
-		password.focus(); 
-		return false; 
-	} 
+	if (lname.value == "")
+	{
+		window.alert("Please enter your name.");
+		name.focus();
+		return false;
+	}
 
-	return true; 
-}</script> 
+	if (email.value == "")
+	{
+		window.alert("Please enter a valid e-mail address.");
+		email.focus();
+		return false;
+	}
+
+	if (number.value == "")
+	{
+		window.alert("Please enter your telephone number.");
+		phone.focus();
+		return false;
+	}
+
+	if (password.value == "")
+	{
+		window.alert("Please enter your password");
+		password.focus();
+		return false;
+	}
+
+	return true;
+}</script>
